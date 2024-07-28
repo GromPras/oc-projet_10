1b932639c62fb360e46af8a16ea4576676681d96
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-07-28 12:49:56 +0200

    validate user on create

--- users/tests.py
+++ users/tests.py
@@ -21,3 +21,33 @@ class UsersTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, "Billy")
+
+    def test_user_registration_validation(self):
+        test_cases = [
+            {
+                "username": "",
+                "password": "password123",
+                "birth_date": "2000-01-01",
+            },
+            {
+                "username": "Billy",
+                "password": "",
+                "birth_date": "2000-01-01",
+            },
+            {
+                "username": "Billy",
+                "password": "password123",
+                "birth_date": "",
+            },
+            {
+                "username": "Billy",
+                "password": "password123",
+                "birth_date": "2000-01-01",
+                "can_be_contacted": "Oui",
+            },
+        ]
+        url = reverse("user-list")
+        for data in test_cases:
+            response = self.client.post(url, data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
